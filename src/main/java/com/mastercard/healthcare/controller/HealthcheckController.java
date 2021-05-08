89d7c00d6c21d5a638134ebcd69bb156d08a3c74
ganesh <pawarganesha1990@gmail.com>
2021-05-08 23:42:15 +0530

    Added bootstrap properties

--- src/main/java/com/mastercard/healthcare/controller/HealthcheckController.java
+++ src/main/java/com/mastercard/healthcare/controller/HealthcheckController.java
@@ -1,14 +1,34 @@
 package com.mastercard.healthcare.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("health")
 public class HealthcheckController {
 
+    @Value("${rate}")
+    String rate;
+
+    @Value("${lanecount}")
+    String count;
+
     @GetMapping("hello")
     public String healthCheck() {
         return "Greetings health is good";
+    }
+
+    @GetMapping("rate")
+    @RefreshScope
+    public String getRate(Model m) {
+        m.addAttribute("rateamount",rate);
+        m.addAttribute("count",count);
+        System.out.println("rate "  + rate);
+        System.out.println("count "  + count);
+
+        return "rateview";
     }
 
 }
